eb0e3160525017ab975af5c1becd0895a9e2ffa1
Alejo902 <alejomurcia902@hotmail.com>
2021-06-10 21:58:27 -0500

    chulo

--- sketch.js
+++ sketch.js
@@ -9,12 +9,12 @@ function draw() {
   fill(255,0,0);
   ellipse(150,150,50,50);
 
-  fill(0);
+  fill(0,255,0);
   ellipse(200,200,50,50);
 
-  fill(255,255,0);
+  fill(255,0,0);
   ellipse(300,300,50,60);
-  
+
 }
 
 
