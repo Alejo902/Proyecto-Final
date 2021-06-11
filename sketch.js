c7c0a87def836624b1dbadeeabb82acad464128f
IsaPinzonV <isabellapv0305@outlook.com>
2021-06-10 22:49:31 -0500

    load.graficos

--- sketch.js
+++ sketch.js
@@ -1,4 +1,16 @@
 
+function preload(){
+  winner=loadImage("/assets/winner.png");
+  raton=loadImage("/assets/raton.png");
+  pag_grande=loadImage("/assets/pag_grande.png");
+  muercie=loadImage("/assets/muercie.png");
+  mabel=loadImage("/assets/mabel.png");
+  hacha=loadImage("/assets/hacha.png");
+  gameO=loadImage("/assets/gameO.png");
+  dipper=loadImage("/assets/dipper.png");
+  diario_gran=loadImage("/assets/diario_gran.png");
+  alfombra=loadImage("/assets/alfombra.png");
+}
 function setup() {
   createCanvas(400, 400);
 }
