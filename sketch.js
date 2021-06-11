d32457cdca7d5a1318f5b5b95d242e280fd83b5c
Vale-pixel <100896964@u.icesi.edu.co>
2021-06-10 22:57:13 -0500

    preload images

--- sketch.js
+++ sketch.js
@@ -1,6 +1,24 @@
+let bill, cinta, fuegoazul, fuegorosa, gnomo, portal, rosa, tienda, tiostan;
+
+function preload(){
+
+bill=loadImage("/assets/0bill0.png");
+cinta=loadImage("/assets/0cinta0.png");
+fuegoazul=loadImage("/assets/0fuegoazul0.png");
+fuegorosa=loadImage("/assets/0fuegorosa0.png");
+gnomo=loadImage("/assets/0gnomo0.png");
+portal=loadImage("/assets/0portal0.png");
+rosa=loadImage("/assets/0rosa0.png");
+tienda=loadImage("/assets/0tienda0.png");
+tiostan=loadImage("/assets/0tiostan0.png");
+
+}
+
+
 function setup() {
   createCanvas(400, 400);
 }
+
 
 function draw() {
   background(220);
