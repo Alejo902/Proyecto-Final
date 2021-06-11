90021d854e5522cb81969fe6749cf56ae44b8860
Vale-pixel <100896964@u.icesi.edu.co>
2021-06-10 23:16:13 -0500

    load again

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
-let bill, cinta, fuegoazul, fuegorosa, gnomo, portal, rosa, tienda, tiostan;
+let bill, cinta, fuegoazul, fuegorosa, gnomo, portal, rosa, tienda, tiostan, winner, raton, pag_grande, muercie, mabel, hacha, gameO, dipper, diario_gran, alfombra;
 
-
-
+function preload(){
 bill=loadImage("/assets/0bill0.png");
 cinta=loadImage("/assets/0cinta0.png");
 fuegoazul=loadImage("/assets/0fuegoazul0.png");
@@ -11,10 +10,20 @@ portal=loadImage("/assets/0portal0.png");
 rosa=loadImage("/assets/0rosa0.png");
 tienda=loadImage("/assets/0tienda0.png");
 tiostan=loadImage("/assets/0tiostan0.png");
+winner=loadImage("/assets/winner.png");
+raton=loadImage("/assets/raton.png");
+pag_grande=loadImage("/assets/pag_grande.png");
+muercie=loadImage("/assets/muercie.png");
+mabel=loadImage("/assets/mabel.png");
+hacha=loadImage("/assets/hacha.png");
+gameO=loadImage("/assets/gameO.png");
+dipper=loadImage("/assets/dipper.png");
+diario_gran=loadImage("/assets/diario_gran.png");
+alfombra=loadImage("/assets/alfombra.png");
 
 
 
-
+}
 function setup() {
   createCanvas(400, 400);
 }
