39b47dcb6f599fa2e7ee3d5d151419472171dbff
Alejo902 <alejomurcia902@hotmail.com>
2021-06-10 23:33:22 -0500

    Load Images

--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
+abuela,corazon,abuelo,cuarto,escogerP,hombreTauro,bosque,paginas,inicial,tronco
+
 function setup() {
   createCanvas(400, 400);
 }
+abuela=loadImage("/assets/Abuela Fantasma.png");
+corazon=loadImage("/assets/CorazónVida.png");
+abuelo=loadImage("/assets/ABUELO FANTASMA.png");
+cuarto=loadImage("/assets/CUARTO.png");
+escogerP=loadImage("/assets/EscogerPersonaje.png");
+hombreTauro=loadImage("/assets/HombreTauro.png");
+bosque=loadImage("/assets/Mapa Bosque.png");
+paginas=loadImage("/assets/Paginas.png");
+inicial=loadImage("/assets/INICIAL.png");
+tronco=loadImage("/assets/Tronco.png");
 
 function draw() {
   background(220);
-  ellipse(100,100,50,50);
-
-  fill(255,0,0);
-  ellipse(150,150,50,50);
-
-  fill(0,255,0);
-  ellipse(150,150,150,150);
-
 
 }
 
